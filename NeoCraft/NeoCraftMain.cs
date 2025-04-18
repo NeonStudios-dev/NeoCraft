b0e86b8078a308f5724aa7b225a5646c7f466094
NeonStudios-dev <info@neonstudios.dev>
2025-04-19 00:26:56 +0200

    New Project called NeoCraft creating Local Minecraft Server

---
+++ NeoCraft/NeoCraftMain.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeoCraftMain
+{
+    public class NeoCraftCli
+    {
+        public void NeoCraft()
+        {
+            Console.WriteLine("NeoCraft method called!");
+            Console.Clear();
+            
+        }
+    }
+}
---
+++ NeoCraft/Program.cs
@@ -0,0 +1,30 @@
+﻿using NeoCraftMain;
+
+namespace NeoCraft
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an instance of NeoCraftMain
+            // Replace with the correct class from the NeoCraftMain namespace
+            var neoCraftMain = new NeoCraftCli(); // Replace 'NeoCraftMainClass' with the actual class name from the NeoCraftMain namespace
+            // Call the NeoCraft method
+            neoCraftMain.NeoCraft();
+            if(File.Exists("FirstRun.txt"))
+            {
+                Console.WriteLine("Checking for first run file...");
+                Thread.Sleep(2000);
+                Console.WriteLine("First run file check complete.");
+                Console.WriteLine("First run file exists.");
+                neoCraftMain.NeoCraft();
+            }
+            else
+            {
+                Console.WriteLine("First run file does not exist.");
+                File.Create("FirstRun.txt").Close();
+                Console.WriteLine("First run file created.");
+        }
+    }
+}
+}
